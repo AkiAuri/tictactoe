165dbb3b982c710d92dd63f1436eeec1b6ceae65
EphiKyue <taifunesenkari@gmail.com>
2025-12-03 00:16:37 +0800

    Uweh

---
+++ app/src/main/java/com/jmccanizares/tictactoe/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.jmccanizares.tictactoe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app. AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_DURATION = 1500; // 1.5 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Hide action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        // Navigate to MainActivity after delay
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DURATION);
+    }
+}
